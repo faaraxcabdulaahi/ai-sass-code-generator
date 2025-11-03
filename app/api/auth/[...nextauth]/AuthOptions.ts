7c743b645d214f91379009d1f696895aecac2074
faaraxcabdulaahi <barashoonline2021@gmail.com>
2025-11-03 14:18:40 +0300

    connected prisma and mongodb in the project

--- app/api/auth/[...nextauth]/AuthOptions.ts
+++ app/api/auth/[...nextauth]/AuthOptions.ts
@@ -1,5 +1,7 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import prisma from "../../../../prisma/client";
 
 export const AuthOptions: NextAuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
@@ -8,8 +10,9 @@ export const AuthOptions: NextAuthOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
       httpOptions: {
-        timeout: 10000, // 10 seconds
+        timeout: 10000, 
       },
     }),
   ],
+  adapter: PrismaAdapter(prisma),
 };
